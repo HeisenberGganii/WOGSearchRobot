44bc07f5849f6a0264508d5cd78c16ea3428f925
HeisenberGganii <162965520+HeisenberGganii@users.noreply.github.com>
2024-03-11 15:54:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  https://github.com/HeisenberGganii/WOGSearchRobot.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /WOGSearchRobot
 fi
-cd /AutoFilterAdvance
+cd /WOGSearchRobot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
